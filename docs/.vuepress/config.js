d8f731432fda69b430b8fe8c8b58fa61466bc41c
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-02-21 19:20:34 -0800

    Revert "First Pushh"
    This reverts commit 8cef6a2e7a24b5a757f1817ebb0e30f2d3abf57c.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  title: 'MC Tecnical Specification',
+  title: 'MC Documentation',
   description: 'A Collaborative Endeavour',
-  base: '/spec/',
+  base: '/docs/',
   head: [
     ['link', { rel: 'shortcut icon', type: 'image/x-icon', href: `favicon.ico` }]
   ],
   themeConfig: {
     nav: [
       { text: 'Intro', link: '/intro/' },
-      { text: 'Github', link: 'https://github.com/DecentralizeJustice/MultisigCryptoTechSpec' },
+      { text: 'Github', link: 'https://github.com/DecentralizeJustice/MultisigCryptoDocumentation' },
       { text: 'Reddit', link: 'https://www.reddit.com/r/DecentralizeJustice' }
     ],
     sidebar: [
